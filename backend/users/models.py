a8fa29705217fcc419c23d4d037f361f51fb8764
ThomasHerreraParra <thomas.herrera@correounivalle.edu.co>
2026-05-21 17:51:47 -0500

    Implementar autenticación JWT y registro usuarios

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('patient', 'Paciente'),
+        ('doctor', 'Medico'),
+        ('admin', 'Administrativo'),
+        ('superadmin', 'Superadministrador'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    document = models.CharField(
+        max_length=20,
+        unique=True
+    )
+
+    eps = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True
+    )
